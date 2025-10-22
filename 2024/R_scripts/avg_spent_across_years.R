10dcd9524ffe4472123c65849c30622c6ee2352e
idaflik <59568380+idaflik@users.noreply.github.com>
2025-10-22 19:49:20 +0200

    Add files via upload

--- 2024/R_scripts/avg_spent_across_years.R
+++ 2024/R_scripts/avg_spent_across_years.R
@@ -9,7 +9,7 @@ per_deportation <- costs_operations %>%
   group_by(year = substr(DATE, 1, 4))%>%
   summarize(EUR = sum(EUR),
             N_RETURNEES = sum(N_RETURNEES))%>%
-  mutate(pp = EUR/N_RETURNEES)%>%
+  mutate(pp = round(EUR/N_RETURNEES , 2))%>%
   rename(eur_total = EUR,
          number_returned = N_RETURNEES,
          avg_eur_per_person = pp)
@@ -20,7 +20,7 @@ per_deportation_country <- costs_operations %>%
   group_by(year = substr(DATE, 1, 4), DEST)%>%
   summarize(EUR = sum(EUR),
             N_RETURNEES = sum(N_RETURNEES))%>%
-  mutate(pp = EUR/N_RETURNEES)%>%
+  mutate(pp = round(EUR/N_RETURNEES, 2))%>%
   rename(eur_total = EUR,
          destination = DEST,
          number_returned = N_RETURNEES,
@@ -29,7 +29,7 @@ per_deportation_country <- costs_operations %>%
 
 write_csv(per_deportation_country, "../clean_data/average_per_year_dest.csv")
 
-returnees %>%
+share_notcounted <- returnees %>%
   group_by(ROID, DEST, DATE)%>%
   summarize(N_RETURNEES = sum(N_RETURNEES, na.rm=T))%>%
   group_by(ROID, DATE)%>%
@@ -37,4 +37,6 @@ returnees %>%
   group_by(year = substr(DATE, 1, 4))%>%
   summarize(doubles = sum(n_dest == 2),
             total = n())%>%
-  mutate(not_counted = doubles/total)
+  mutate(not_counted = round(doubles/total),2)
+
+write_csv(share_notcounted, "../clean_data/averages_omitted.csv")
